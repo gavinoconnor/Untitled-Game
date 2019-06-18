da3fce209da6a4d0aebcef4f67cdc8e5aeecf3f3
gavinoconnor <gedmund@gmail.com>
2019-06-18 18:16:26 -0400

    made small fixes

--- server/public/js/Preload.js
+++ server/public/js/Preload.js
@@ -18,41 +18,38 @@ const preloadState = new Phaser.Class({
       this.load.image("bg_1", "assets/bg-1.png");
       this.load.image("bg_2", "assets/bg-2.png");
       this.load.image("ground", "assets/ground.png");
+
       // load player spritesheet
       this.load.spritesheet("player", "assets/bee.png",
       {
         frameWidth: 37,
         frameHeight: 39
       });
-<<<<<<< HEAD
+
       this.load.spritesheet("bat", "assets/bat.png",{
-        frameWidth: 14,
-        frameHeight: 14
+        frameWidth: 16,
+        frameHeight: 16
       });
+
       this.load.spritesheet("ghost", "assets/ghost.png",{
         frameWidth: 16,
         frameHeight: 16
       });
+
       this.load.spritesheet("skeleton", "assets/skeleton.png",{
         frameWidth: 16,
         frameHeight: 16
       });
+
       this.load.spritesheet("explosion", "assets/explosion.png",{
         frameWidth: 16,
         frameHeight: 16
       });
+
       this.load.spritesheet("beam", "assets/items.png",{
       frameWidth: 16,
       frameHeight: 16
       });
-=======
-
-      this.load.spritesheet("bat", "assets/bat.png",
-        {
-          frameWidth: 16,
-          frameHeight: 16
-        });
->>>>>>> sounds
     },
 
     create: function() {
